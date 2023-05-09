cc8bb65c3d5f0d563873c035517985f49c12285d
halws <Kotsuyba@gmail.com>
2023-05-09 13:25:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-zcr3qz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-zcr3qz)
---
+++ server/api/[...].js
@@ -0,0 +1,4 @@
+export default defineEventHandler((event) => {
+  console.log(event);
+  return $fetch(`https://api.coinlore.net${event.node.req.url}`);
+});
